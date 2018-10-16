a949c1bc7d273da7c581ec5574d97e1c77eadd34
Florian Richter <floririchte@gmail.com>
2018-10-16 13:09:52 +0200

    Fix tests running in CI

--- packages/backend/tests/examples/birthdays.test.ts
+++ packages/backend/tests/examples/birthdays.test.ts
@@ -4,6 +4,7 @@ import { doTestWithDb } from '../test-utils';
 describe('Birthdays Example', () => {
   test('should pass through', () =>
     doTestWithDb(async db => {
+      jest.setTimeout(30000);
       const res = await createBirthdaysDemoData({ db, userId: '' });
       expect(res).toBe(true);
     }));
